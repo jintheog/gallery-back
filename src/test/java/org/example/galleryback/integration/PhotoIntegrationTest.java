3281b248973ae0c212183417e10f1894a31731eb
jintheog <jintheog11@gmail.com>
2026-01-20 10:58:45 +0900

    sdf

---
+++ src/test/java/org/example/galleryback/integration/PhotoIntegrationTest.java
@@ -0,0 +1,45 @@
+package org.example.galleryback.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.example.galleryback.entity.Photo;
+import org.example.galleryback.repository.PhotoRepository;
+import org.example.galleryback.service.PhotoService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@RequiredArgsConstructor
+public class PhotoIntegrationTest {
+    private final MockMvc mockMvc;
+    private final PhotoRepository photoRepository;
+    private final ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("사진목록조회")
+    void getPhotos() throws Exception {
+        photoRepository.save(
+                Photo.builder().title("image1").imageUrl("1.jpg").build()
+        );
+        photoRepository.save(
+                Photo.builder().title("image2").imageUrl("2.jpg").build()
+        );
+
+        mockMvc.perform(get("/api/photos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value("image1"));
+    }
+
+}
